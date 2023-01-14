a405eb6f44927873a1c1a6c98f11579cb76054b6
Filipe Fernandes <80523799+filipefernandesdev@users.noreply.github.com>
2023-01-14 22:54:33 +0000

    Updated the website navigation
    + Header
    + Page with all cryptocurrencies (basic info)
    + Page with each cryptocurrency's specific data

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from binance.spot import Spot
+from newsapi import NewsApiClient
 from datetime import datetime, timedelta
 # import requests # Import API
 views = Blueprint(__name__, "views")
@@ -10,16 +11,45 @@ def index():
     return render_template("index.html")
 
 
-@views.route("/crypto/prices")
-def get_prices():
+@views.route("/crypto")
+def get_all_prices():
     # Query Parameters
     args = request.args
-    baseAsset = args.get("base")
-    if baseAsset == None:
-        baseAsset = "BTC"
-    quoteAsset = args.get("quote")
+    quoteAsset = args.get("q")
     if quoteAsset == None:
-        quoteAsset = "USDT"
+        quoteAsset = "EUR"
+
+    # Binance Library
+    client = Spot()
+
+    # Available baseAssets
+    baseAsset = ["BTC", "ETH", "XRP", "BNB", "XLM"]
+
+    # Cryptos
+    symbol = []
+    for i in baseAsset:
+        symbol.append(i+quoteAsset)
+
+    # Current Price
+    price = []
+    for i in symbol:
+        price.append(float((client.ticker_price(i))["price"]))
+
+    # Price 24 hours (%)
+    price24h = []
+    for i in symbol:
+        price24h.append(float((client.ticker_24hr(i))["priceChangePercent"]))
+
+    return render_template("crypto.html", baseAsset=baseAsset, quoteAsset=quoteAsset, symbol=symbol, price=price, price24h=price24h)
+
+
+@views.route("/crypto/<baseAsset>")
+def get_specific_data(baseAsset):
+    # Query Parameters
+    args = request.args
+    quoteAsset = args.get("q")
+    if quoteAsset == None:
+        quoteAsset = "EUR"
 
     # Symbol
     symbol = baseAsset + quoteAsset
@@ -53,4 +83,4 @@ def get_prices():
         formattedDate = currentDate.strftime('%d-%m-%Y')
         dateList.append(formattedDate)
 
-    return render_template("prices.html", baseAsset=baseAsset, quoteAsset=quoteAsset, symbol=symbol, price=price, price24h=price24h, priceHistory_open=priceHistory_open, priceHistory_high=priceHistory_high, priceHistory_low=priceHistory_low, priceHistory_close=priceHistory_close, dateList=dateList)
+    return render_template("crypto_specific.html", baseAsset=baseAsset, quoteAsset=quoteAsset, symbol=symbol, price=price, price24h=price24h, priceHistory_open=priceHistory_open, priceHistory_high=priceHistory_high, priceHistory_low=priceHistory_low, priceHistory_close=priceHistory_close, dateList=dateList)
